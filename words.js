bc2b8a76f1ab692b5501971b5f36e15fb70554cb
Mariana West <marianahartenthal@gmail.com>
2022-07-14 11:20:29 +0100

    first commit

---
+++ words.js
@@ -0,0 +1,14 @@
+export const WORDS = [
+    'kozak',
+    'lange',
+    'moure',
+    'maack',
+    'peret',
+    'borba',
+    'wanda',
+    'hanke',
+    'bororo',
+    'xeta', 
+    'parana', 
+    'mate', 
+  ]
